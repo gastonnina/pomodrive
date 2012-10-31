4e8586a5072296db95a3653dfc093ff502da196d
Gaston Nina <gastonnina@gmail.com>
2012-10-30 23:19:20 -0400

    2do Splash con Estilos

---
+++ src/com/gastonnina/pomodrive/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.gastonnina.pomodrive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+	
+}
