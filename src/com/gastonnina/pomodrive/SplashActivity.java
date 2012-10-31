3ff74f627480925f5c22bb1003568e31a8d0811a
Gaston Nina <gastonnina@gmail.com>
2012-10-30 23:46:07 -0400

    Poniendo Animacion

--- src/com/gastonnina/pomodrive/SplashActivity.java
+++ src/com/gastonnina/pomodrive/SplashActivity.java
@@ -2,6 +2,9 @@ package com.gastonnina.pomodrive;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
@@ -9,6 +12,16 @@ public class SplashActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        iniciarAnimacion();
+
     }
-	
+    /**
+     * Animacion de Splash
+     */
+    public void iniciarAnimacion(){
+    	TextView txt = (TextView)findViewById(R.id.textSplahTitle);
+    	Animation animacion= AnimationUtils.loadAnimation(this, R.anim.animacion);
+    	txt.startAnimation(animacion);
+	}
+
 }
